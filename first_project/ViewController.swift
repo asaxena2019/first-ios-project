572e5c8e1e02604cf20e92ef649a91c9c4854569
asaxena2019 <anushkasaxena2019@gmail.com>
2020-05-20 15:30:01 -0400

    first time using labels with strings

--- first_project/ViewController.swift
+++ first_project/ViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var myName: String = "Saxena"
+    var myName: String = "Anushka"
+    @IBOutlet weak var myLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myName = "Anushka Saxena"
+        myLabel.text = myName
     }
 
 
 }
-
