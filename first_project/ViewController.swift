ed46256d4252b0593d0880a486932b17b8ba6d82
asaxena2019 <anushkasaxena2019@gmail.com>
2020-05-27 13:19:32 -0400

    toggle lights

--- first_project/ViewController.swift
+++ first_project/ViewController.swift
@@ -8,31 +8,18 @@
 
 import UIKit
 
-enum modes {
-    case notSet
-    case addition
-    case subtraction
-}
-
 class ViewController: UIViewController {
     
-    @IBOutlet weak var myLabel: UILabel!
+    var isOn: Bool = true
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.s
-        let mode: modes = .notSet
-        let num1: Int = 5
-        let num2: Int = 4
-        var total: Int = 0
-        if mode == .addition {
-            total = num1 + num2
+    @IBAction func toggleLights(_ sender:Any){
+        if isOn == true {
+            isOn = false
+            view.backgroundColor = .darkGray
         }
-        else if mode == .subtraction{
-            total = num1 - num2
+        else {
+            isOn = true
+            view.backgroundColor = .white
         }
-        myLabel.text = "Total is \(total)"
     }
-
-
 }
